1fbb118709ad695ab857472ff91ea50fc29ec7c4
nitesh <nitesh9501@hotmail.com>
2022-12-14 12:51:34 +0530

    Replace .then.catch handlera with try-catch statements and add addProductToCart and removeProductFromCart, controllers/cart.js

--- controllers/cart.js
+++ controllers/cart.js
@@ -2,53 +2,62 @@ import { generalizeResult } from "../libs/mongoose.js";
 import models from "../models/_models.js";
 const { Cart } = models;
 
-function getCarts(req, res) {
-  Cart.find()
-    .then((carts) => {
-      carts = generalizeResult(carts);
-      res.json(carts);
-    })
-    .catch((e) => {
-      res.status(500).json({});
-    });
+async function getCarts(req, res) {
+  try {
+    const carts = await Cart.find();
+    res.status(200).json(generalizeResult(carts));
+  } catch (e) {
+    res.status(500).json({});
+  }
 }
 
-function addCart(req, res) {
-  Cart.create(req.body)
-    .then((cart, errors) => {
-      if (cart) {
-        res.status(200).json(generalizeResult(cart));
-      } else {
-        res.status(422).json(errors);
-      }
-    })
-    .catch((e) => {
-      res.status(500).json({});
-    });
+async function addCart(req, res) {
+  try {
+    const cart = await Cart.create(req.body);
+    res.status(200).json(generalizeResult(cart));
+  } catch (e) {
+    res.status(500).json({});
+  }
 }
 
-function getCart(req, res) {
-  Cart.findById(req.params.cartId)
-    .then((cart) => {
-      res.status(200).json(generalizeResult(cart));
-    })
-    .catch((e) => {
-      res.status(500).json({});
-    });
+async function getCart(req, res) {
+  try {
+    const cart = await Cart.findOne({ _id: req.params.cartId });
+    res.status(200).json(generalizeResult(cart));
+  } catch (e) {
+    res.status(500).json({});
+  }
 }
 
-function updateCart(req, res) {
-  Cart.findOneAndUpdate({ _id: req.params.cartId }, { $set: req.body }, { new: true })
-    .then((cart, errors) => {
-      if (cart) {
-        res.status(200).json(generalizeResult(cart));
-      } else {
-        res.status(422).json(errors);
-      }
-    })
-    .catch((e) => {
-      res.status(500).json({});
-    });
+async function addProductToCart(req, res) {
+  try {
+    const cartId = req.params.cartId;
+    const cart = await Cart.findOne({_id: cartId});
+    const updated = await Cart.findOneAndUpdate(
+      { _id: req.params.cartId },
+      { $set: {products: [...cart.products, req.body.product]} },
+      { new: true }
+    );
+    res.status(200).json(generalizeResult(updated));
+  } catch (e) {
+    res.status(500).json({});
+  }
 }
 
-export default { getCarts, addCart, getCart, updateCart };
+async function removeProductFromCart(req, res) {
+  try {
+    const cartId = req.params.cartId;
+    const cart = await Cart.findOne({_id: cartId});
+    const products = cart.products.filter((product) => product.toString() !== req.body.product);
+    const updated = await Cart.findOneAndUpdate(
+      { _id: req.params.cartId },
+      { $set: {products: products} },
+      { new: true }
+    );
+    res.status(200).json(generalizeResult(updated));
+  } catch (e) {
+    res.status(500).json({});
+  }
+}
+
+export default { getCarts, addCart, getCart, addProductToCart, removeProductFromCart };
