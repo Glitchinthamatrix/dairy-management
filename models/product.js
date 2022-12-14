73530d356cf66b46442b228ed9334af5c1cb48f7
nitesh <nitesh9501@hotmail.com>
2022-12-14 22:53:17 +0530

    Remove default brand and default category from product schema

--- models/product.js
+++ models/product.js
@@ -4,12 +4,11 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
   title: { type: String, required: [true, "Title is required"] },
   description: { type: String, default: null },
-  brand: { type: Schema.Types.ObjectId, ref: "Brand", default: "63997b643955a913e2a9684a" },
+  brand: { type: Schema.Types.ObjectId, ref: "Brand" },
   price: { type: Number, required: [true, "Price is required"] },
   category: {
     type: Schema.Types.ObjectId,
     ref: "Category",
-    default: "6392fb2765a307bdd2e62c7f",
   },
   unitsAvailable: { type: Number, default: Number.MAX_SAFE_INTEGER },
   images: { type: [String], caste: false },
