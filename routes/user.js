ddb66209f0bfeb64144885631bce2c9b53b8c552
nitesh <nitesh9501@hotmail.com>
2022-12-14 15:04:28 +0530

    Authenticate request in /me route

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 import controllers from "../controllers/_controllers.js";
 import schemaEnforcers from "../schema-enforcers/_schema-enforcers.js";
-const { userController } = controllers;
+const { authController, userController } = controllers;
 const { userSchemaEnforcer } = schemaEnforcers;
 
 router
@@ -11,7 +11,9 @@ router
   .post(userSchemaEnforcer, userController.addUser);
 
 // Do not move /me route down, it will be called inside /:id route with 'me' as id
-router.route("/me").get(userController.getSelf);
+router
+  .route("/me")
+  .get(authController.verifyUserAndPassAsResponseLocal, userController.getSelf);
 
 router
   .route("/:userId")
