ca044c9548f2654d663792c0de9f14115f6d892d
nitesh <nitesh9501@hotmail.com>
2022-12-15 15:12:33 +0530

    Fix generalizeResult

--- libs/mongoose.js
+++ libs/mongoose.js
@@ -1,35 +1,41 @@
 import mongoose from "mongoose";
 import { isObject } from "../utils/object.js";
 
-function convertNestedMongooseDocumentsToObjects(document) {
-  if (document === null) {
-    return null;
-  } else if (Array.isArray(document)) {
-    generalizeResult(document);
-  } else {
-    for (let key in document) {
-      if (isObject(document[key]) && !(document[key] instanceof mongoose.Types.ObjectId)) {
-        convertNestedMongooseDocumentsToObjects(document[key]);
-      }
+function generalizeArray(array) {
+  for (let document of array) {
+    generalizeObject(document);
+  }
+}
+
+function generalizeObject(object) {
+  if (!isObject(object) || object instanceof mongoose.Types.ObjectId) {
+    return;
+  }
+  if (object._id) {
+    object.id = object._id;
+    delete object._id;
+    delete object.__v;
+  }
+  generalizeNestedValues(object);
+}
+
+function generalizeNestedValues(object) {
+  for (let key in object) {
+    if (Array.isArray(object[key])) {
+      generalizeArray(object[key]);
+    } else if (isObject(object[key])) {
+      generalizeObject(object[key]);
     }
-    document.id = document._id.toString();
-    delete document.__v;
-    delete document._id;
   }
 }
 
 export function generalizeResult(result) {
-  if(result === null || result === undefined){
-    return result;
-  }
   if (Array.isArray(result)) {
     result = result.map((item) => item.toObject());
-    for (let item of result) {
-      convertNestedMongooseDocumentsToObjects(item);
-    }
-  } else {
+    generalizeArray(result);
+  } else if (result instanceof mongoose.Document) {
     result = result.toObject();
-    convertNestedMongooseDocumentsToObjects(result);
+    generalizeObject(result);
   }
   return result;
 }
