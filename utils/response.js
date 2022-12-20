5ed8a821dae3b745ca4dff041bdee0f1c1d6ef26
nitesh <nitesh9501@hotmail.com>
2022-12-20 13:49:55 +0530

    Fix utility function 'sendFileStream'

--- utils/response.js
+++ utils/response.js
@@ -1,4 +1,6 @@
-export function sendFileStream(res, path) {
+import { ERROR_CODE_FILE_NOT_FOUND } from "../constants.js";
+
+function _streamFileContent(res, path) {
   return new Promise((resolve, reject) => {
     res.sendFile(path, (err) => {
       if (err) {
@@ -8,4 +10,16 @@ export function sendFileStream(res, path) {
       }
     });
   });
+}
+
+export async function sendFileStream(res, path) {
+  try {
+    await _streamFileContent(res, path);
+  } catch (e) {
+    if (e.code === ERROR_CODE_FILE_NOT_FOUND) {
+      res.status(404).json({});
+    } else {
+      res.status(500).json({});
+    }
+  }
 }
