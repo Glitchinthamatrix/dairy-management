08abefe37041d277d3e824b42edfcdb941be65d1
nitesh <nitesh9501@hotmail.com>
2022-12-20 17:56:25 +0530

    Autheticate request before making changes to wishlist

--- routes/wishlist.js
+++ routes/wishlist.js
@@ -2,19 +2,27 @@ import express from "express";
 const router = express.Router();
 import controllers from "../controllers/_controllers.js";
 import schemaEnforcers from "../schema-enforcers/_schema-enforcers.js";
-import {validateUpdateWishlistProductsRequestBody} from "../utils/request.js";
-const { wishlistController } = controllers;
+import { validateUpdateWishlistProductsRequestBody } from "../utils/request.js";
+const { authController, wishlistController } = controllers;
 const { wishlistSchemaEnforcer } = schemaEnforcers;
 
 router
   .route("/")
-  .get(wishlistController.getWishlists)
+  .get(authController.verifyUserAndPassAsResponseLocal, wishlistController.getWishlists)
   .post(wishlistSchemaEnforcer, wishlistController.addWishlist);
 
 router
   .route("/:wishlistId")
-  .get(wishlistController.getWishlist)
-  .put(validateUpdateWishlistProductsRequestBody, wishlistController.addProductToWishlist)
-  .delete(validateUpdateWishlistProductsRequestBody, wishlistController.removeProductFromWishlist);
+  .get(authController.verifyUserAndPassAsResponseLocal, wishlistController.getWishlist)
+  .put(
+    authController.verifyUserAndPassAsResponseLocal,
+    validateUpdateWishlistProductsRequestBody,
+    wishlistController.addProductToWishlist
+  )
+  .delete(
+    authController.verifyUserAndPassAsResponseLocal,
+    validateUpdateWishlistProductsRequestBody,
+    wishlistController.removeProductFromWishlist
+  );
 
 export default router;
