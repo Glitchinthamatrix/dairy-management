b9a2168407a1701af660ba84a34abbe74b63b379
nitesh <nitesh9501@hotmail.com>
2022-12-21 15:52:43 +0530

    Add attributes in order schema

--- models/order.js
+++ models/order.js
@@ -4,6 +4,10 @@ const Schema = mongoose.Schema;
 const orderSchema = new Schema({
   products: { type: [Schema.Types.ObjectId], ref: "Product" },
   user: { type: Schema.Types.ObjectId, ref: "User", required: ["User is required"] },
+  isDelivered: { type: Boolean, default: false },
+  transaction: { type: Schema.Types.ObjectId, default: null },
+  deliveryDate: { type: Date, required: [true, "Delivery date is required"] },
+  deliveryAddress: { type: String, required: [true, "Delivery address is required"] },
 });
 
 const model = mongoose.model("Order", orderSchema);
