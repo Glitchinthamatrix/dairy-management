82dd1f8abe08f15c55b35cbe31ed03ed19344206
nitesh <nitesh9501@hotmail.com>
2022-12-21 15:52:20 +0530

    Set default values in product schema

--- models/product.js
+++ models/product.js
@@ -4,16 +4,17 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
   title: { type: String, required: [true, "Title is required"] },
   description: { type: String, default: null },
-  brand: { type: Schema.Types.ObjectId, ref: "Brand" },
+  brand: { type: Schema.Types.ObjectId, ref: "Brand", default: null },
   price: { type: Number, required: [true, "Price is required"] },
   category: {
     type: Schema.Types.ObjectId,
     ref: "Category",
+    default: null
   },
   unitsAvailable: { type: Number, default: Number.MAX_SAFE_INTEGER },
-  images: { type: [String], caste: false },
+  images: { type: [String] },
   reviews: { type: [Schema.Types.ObjectId], ref: "Review" },
-  addedBy: { type: Schema.Types.ObjectId, ref: "User" },
+  addedBy: { type: Schema.Types.ObjectId, ref: "User", required: [true, "Seller is required"] },
 });
 
 const model = mongoose.model("Product", productSchema);
