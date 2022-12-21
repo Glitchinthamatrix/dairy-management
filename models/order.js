f52b8e7b6ce0a54559eb020297b8988fa6234fff
nitesh <nitesh9501@hotmail.com>
2022-12-21 16:42:04 +0530

    Fix order schema

--- models/order.js
+++ models/order.js
@@ -5,7 +5,7 @@ const orderSchema = new Schema({
   product: { type: Schema.Types.ObjectId, ref: "Product", required: [true, "Product is required"] },
   user: { type: Schema.Types.ObjectId, ref: "User", required: ["User is required"] },
   isDelivered: { type: Boolean, default: false },
-  transaction: { type: Schema.Types.ObjectId, default: null },
+  transaction: { type: Schema.Types.ObjectId, ref: "Transaction", default: null },
   deliveryDate: { type: Date, required: [true, "Delivery date is required"] },
   deliveryAddress: { type: String, required: [true, "Delivery address is required"] },
 });
