ac4042fe7f3b8c50361bad8a51c7c08271a35ea3
nitesh <nitesh9501@hotmail.com>
2022-12-26 00:46:28 +0530

    Authenticate admin before adding category

--- routes/category.js
+++ routes/category.js
@@ -2,13 +2,18 @@ import express from "express";
 const router = express.Router();
 import controllers from "../controllers/_controllers.js";
 import schemaEnforcers from "../schema-enforcers/_schema-enforcers.js";
-const { categoryController } = controllers;
+const { authController, categoryController } = controllers;
 const { categorySchemaEnforcer } = schemaEnforcers;
 
 router
   .route("/")
   .get(categoryController.getCategories)
-  .post(categorySchemaEnforcer, categoryController.addCategory);
+  .post(
+    authController.verifyUserAndPassAsResponseLocal,
+    authController.verifyAdminFromResponseLocals,
+    categorySchemaEnforcer,
+    categoryController.addCategory
+  );
 
 router.route("/:categoryId").delete(categoryController.removeCategory);
 
