070dcaff0ca68f5861da7aaa0c0633580c3ce5a4
nitesh <seveneyedeagle@gmail.com>
2023-01-01 19:51:45 +0530

    Format utils

--- utils/request.js
+++ utils/request.js
@@ -8,40 +8,40 @@ const updateWishlistProductsRequestBodySchema = {
   product: { type: MONGOOSE_ID.type, required: true },
 };
 
-export function validateUpdateCartProductsRequestBody (req, res, next) {
+export function validateUpdateCartProductsRequestBody(req, res, next) {
   return schemaEnforcer({
     req: req,
     res: res,
     next: next,
     modelProperties: updateCartProductsRequestBodySchema,
   });
-};
+}
 
-export function validateUpdateWishlistProductsRequestBody (req, res, next) {
+export function validateUpdateWishlistProductsRequestBody(req, res, next) {
   return schemaEnforcer({
     req: req,
     res: res,
     next: next,
     modelProperties: updateWishlistProductsRequestBodySchema,
   });
-};
-
-const reviewRequestBodySchema = {
-  comment: {type: STRING.type, required: true},
 }
 
-export function validateProductReviewRequestBody (req, res, next) {
+const reviewRequestBodySchema = {
+  comment: { type: STRING.type, required: true },
+};
+
+export function validateProductReviewRequestBody(req, res, next) {
   return schemaEnforcer({
     req: req,
     res: res,
     next: next,
     modelProperties: reviewRequestBodySchema,
   });
-};
+}
 
 export function verifyParamAssociationToUser(req, res, next, map) {
-  for(let param in map){
-    if(req.params[param] !== res.locals.user[map[param]]){
+  for (let param in map) {
+    if (req.params[param] !== res.locals.user[map[param]]) {
       res.status(401).json({});
       return;
     }
